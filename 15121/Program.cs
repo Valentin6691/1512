a7c9691c2023f4ab9811c7d54f632dafdb4f6c5d
Valentin <nitnelav6691@gmail.cov>
2023-12-15 12:41:39 +0300

    1512_1

---
+++ 15121/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве. Программа 
+// должна выдать ответ: Да/Нет.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[5];
+
+        bool isFind = false;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(1, 10);
+            Console.Write(array[i] + " ");
+
+            if (array[i] == num)
+            {
+                isFind = true;
+            }
+        }
+        if (isFind == true)
+        {
+            Console.Write("Да");
+        }
+        else
+        {
+            Console.Write("Нет");
+        }
+    }
+}
